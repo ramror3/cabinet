51a8560800343c96c8c5b96913bf7d43450c17ec
ramror <ramesh.ramror@gmail.com>
2018-07-10 14:19:14 +0530

    finale commit

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
   resources :docs
+  authenticated :user do 
+    root "docs#index", as: "authenticated_root"
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
